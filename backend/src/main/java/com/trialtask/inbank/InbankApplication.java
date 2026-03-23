451dfed1e92eac421a01e097127aa5347b0ae9c9
arvysodev <artjom.vysotski@devtailor.com>
2026-03-23 13:36:04 +0200

    init backend skeleton

---
+++ backend/src/main/java/com/trialtask/inbank/InbankApplication.java
@@ -0,0 +1,13 @@
+package com.trialtask.inbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InbankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InbankApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/trialtask/inbank/InbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trialtask.inbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
